e5873000c0c1fbad0dfc89190921edddabf507f9
MarioRgzB <mariob.rgzbvdes97@gmail.com>
2025-08-11 21:28:01 -0600

    Update Ejercicio1.math.c

--- semana1/Ejercicio1.math.c
+++ semana1/Ejercicio1.math.c
@@ -7,7 +7,7 @@ int main() {
   y=2.3;
   z=6.4;
   a=1.2;
-  b=8.1
+  b=8.1;
   printf("%f %f %f \n",pow(x,2),log(x+1),exp(x));
   printf("%f %f %f \n",pow(y,2),log(y+1),exp(y));
   printf("%f %f %f \n",pow(z,2),log(z+1),exp(z));

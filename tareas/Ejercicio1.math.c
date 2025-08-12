ab65df03bdde88cb47829f021711b5a015004df3
MarioRgzB <mariob.rgzbvdes97@gmail.com>
2025-08-12 10:16:15 -0600

    Add files via upload

---
+++ tareas/Ejercicio1.math.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  float x,y,z,a,b;
+  x=4.6;
+  y=2.3;
+  z=6.4;
+  a=1.2;
+  b=8.1;
+  printf("%f %f %f \n",pow(x,2),log(x+1),exp(x));
+  printf("%f %f %f \n",pow(y,2),log(y+1),exp(y));
+  printf("%f %f %f \n",pow(z,2),log(z+1),exp(z));
+  printf("%f %f %f \n",pow(a,2),log(a+1),exp(a));
+  printf("%f %f %f \n",pow(b,2),log(b+1),exp(b));
+
+}

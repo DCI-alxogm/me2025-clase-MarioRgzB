082128ea879a94d9b65d8216e762375b71f226d7
MarioRgzB <mariob.rgzbvdes97@gmail.com>
2025-11-11 23:28:55 -0600

    Update Proyecto_sistemasL.c

--- tareas/Proyecto_sistemasL.c
+++ tareas/Proyecto_sistemasL.c
@@ -10,3 +10,19 @@
  void solucionSistemaLU(double L[][ORDEN_MATRIZ], double U[][ORDEN_MATRIZ], double b[], double x[], int orden);
  void iteracionGS(double A[][ORDEN_MATRIZ], double b[], double x[], int orden, int maxIt, double tl);
  void eliminacionGJ(double A[][ORDEN_MATRIZ], double b[], double x[], int orden);
+// Función principal int main
+ int main() {
+     // Matriz A de coeficientes 
+     double matrizA[ORDEN_MATRIZ][ORDEN_MATRIZ] = {
+         {-150, 244.5, 0, 0, 0},
+         {100, -344.5, 325.5, 0, 0},
+         {0, 100, -525.5, 405, 0},
+         {0, 0, 200, -605, 499.5},
+         {0, 0, 0, 200, -549.5}
+     };
+     //Vector de términos independientes b
+     double vectorB[ORDEN_MATRIZ] = {0, 0, -30, 0, 0};
+     // Vectores que contienen las soluciones obtenidas por los 3 métodos
+     double solucionLU[ORDEN_MATRIZ], solucionGaussSeidel[ORDEN_MATRIZ], solucionGaussJordan[ORDEN_MATRIZ];
+     // solución por factorización LU
+     double matrizL[ORDEN_MATRIZ][ORDEN_MATRIZ], matrizU[ORDEN_MATRIZ][ORDEN_MATRIZ];
